8e452bcd7e2559bf46e26d3b921eca92f8247ed5
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-03-21 16:26:35 -0300

    M  .gitignore M  composer.json M  composer.lock M  config/app.php D  storage/app/.gitignore D  storage/app/public/.gitignore

--- config/app.php
+++ config/app.php
@@ -176,6 +176,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //cusom
+        BackupManager\Laravel\Laravel55ServiceProvider::class,
 
     ],
 
